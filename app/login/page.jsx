ef8f89d833de1cf7ec3e8ba051d0c170729ff437
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-09 14:32:29 +0300

    login page intro-container eklendi

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,7 +3,9 @@ import { login, signup } from "./actions";
 import "./login.css";
 export default function LoginPage() {
   return (
+    <div className="container">
     <form>
+      <h2>Giriş Yap</h2>
       <input
         id="email"
         name="email"
@@ -21,5 +23,14 @@ export default function LoginPage() {
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
+    <div className="intro-container">
+      <h2>Admin Paneline Hoşgeldiniz</h2>
+      <p>
+      Yönetim paneline erişim sağlamak için lütfen giriş yapın.
+      Eğer bir hesabınız yoksa, yeni bir yönetici hesabı oluşturabilirsiniz.
+      </p>
+      <button>No account yet? Signup.</button>
+      </div>
+    </div>
   );
 }
