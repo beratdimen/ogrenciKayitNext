beab0c8b0f6709332b78817c9b42281f5c3abb4f
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-09 14:34:58 +0300

    login page düzenlendi

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,13 +3,35 @@ import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="container">
+      <form>
+        <h2>Giriş Yap</h2>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          placeholder="E-Posta Adresi"
+        />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          placeholder="Şifre"
+        />
+        <button formAction={login}>Giriş Yap</button>
+        <button formAction={signup}>Kayıt Ol</button>
+      </form>
+      <div className="intro-container">
+        <h2>Admin Paneline Hoşgeldiniz</h2>
+        <p>
+          Yönetim paneline erişim sağlamak için lütfen giriş yapın.
+          Eğer bir hesabınız yoksa, yeni bir yönetici hesabı oluşturabilirsiniz.
+        </p>
+        <button>No account yet? Signup.</button>
+      </div>
+    </div>
   );
 }
+
