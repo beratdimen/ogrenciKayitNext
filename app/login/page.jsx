b0e668016d12f42b49f9be8ef0d82e2a7025083a
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-09 15:14:25 +0300

    merge with master

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,15 +3,34 @@ import { login, signup } from "./actions";
 import "./login.css";
 export default function LoginPage() {
   return (
+    <div className="container">
     <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <h2>Giriş Yap</h2>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        required
+        placeholder="E-Posta Adresi"
+      />
+      <input
+        id="password"
+        name="password"
+        type="password"
+        required
+        placeholder="Şifre"
+      />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
+    <div className="intro-container">
+      <h2>Admin Paneline Hoşgeldiniz</h2>
+      <p>
+      Yönetim paneline erişim sağlamak için lütfen giriş yapın.
+      Eğer bir hesabınız yoksa, yeni bir yönetici hesabı oluşturabilirsiniz.
+      </p>
+      <button>No account yet? Signup.</button>
+      </div>
+    </div>
   );
 }
-console.log("Mevcut değişiklik");
-
