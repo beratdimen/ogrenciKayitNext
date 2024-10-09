34663c585c8e65155a1f161087086c78628b7cce
Berat Dimen <beratefedimen@hotmail.com>
2024-10-09 14:18:25 +0300

    input düzenlendi

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,10 +4,20 @@ import "./login.css";
 export default function LoginPage() {
   return (
     <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        required
+        placeholder="E-Posta Adresi"
+      />
+      <input
+        id="password"
+        name="password"
+        type="password"
+        required
+        placeholder="Şifre"
+      />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
