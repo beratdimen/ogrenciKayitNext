afbc69adaa1565af48ca6b57dfa24f0f166b055c
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-11 21:16:47 +0300

    tableHead.css dosyası oluşturuldu

--- components/students/index.jsx
+++ components/students/index.jsx
@@ -2,11 +2,13 @@
 import { UserIcon } from "@/helpers/icons";
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export default function StudentsPage() {
   const supabase = createClient();
   const [students, setStudents] = useState([]);
   const [loading, setLoading] = useState([]);
+
   const fetchStudents = async () => {
     let { data, error } = await supabase.from("students").select("*");
     if (error) {
@@ -19,8 +21,17 @@ export default function StudentsPage() {
   useEffect(() => {
     fetchStudents();
   }, []);
+
+  const handleLogout = async () => {
+    toast.success("Çıkış Yapıldı");
+    await supabase.auth.signOut();
+    window.location.reload(false);
+  };
+
   return (
     <div>
+      <button onClick={() => handleLogout()}>Çıkış Yap</button>
+
       {students.length > 0 ? (
         students.map((student) => (
           <div key={student.id}>
