5276772c81b325096f592d5814899f7fc84a80a9
Berat Dimen <beratefedimen@hotmail.com>
2024-10-11 23:08:20 +0300

    css

--- components/students/index.jsx
+++ components/students/index.jsx
@@ -3,9 +3,7 @@ import { UserIcon } from "@/helpers/icons";
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import '../studentsForm/studentsForm.css';
-
-
+import "./students.css";
 
 export default function StudentsPage() {
   const supabase = createClient();
